ae4a08a4d7c3b7b81ff74054a0a4ba18618dc454
Binur <b_akhmet@kbtu.kz>
2026-04-19 17:35:55 +0500

    Add files via upload

---
+++ Serpin-tau/frontend/src/app.module.ts
@@ -0,0 +1,21 @@
+// src/app/app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoreModule,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
