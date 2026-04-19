0a5c92b75511274b6301ee0aedb586d8453e6cee
A1uaa <al_akhmetzhanova@kbtu.kz>
2026-04-19 17:19:30 +0500

    Add files via upload

---
+++ Serpin-tau/frontend/src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+// src/app/core/core.module.ts
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent]
+})
+export class CoreModule { }
---
+++ Serpin-tau/frontend/src/app/core/header/header.component.ts
@@ -0,0 +1,39 @@
+// src/app/core/header/header.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterModule], 
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  currentUser: User | null = null;
+  isMenuOpen = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.isMenuOpen = false;
+  }
+
+  isOrganizer(): boolean {
+    return this.authService.isOrganizer();
+  }
+}
